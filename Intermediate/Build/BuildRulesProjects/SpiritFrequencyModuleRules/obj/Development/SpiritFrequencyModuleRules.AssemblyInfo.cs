678a15e12327f44968ac35f98fc0d0719a7bdda6
Nora Vidström <144795280+NoraVstrom@users.noreply.github.com>
2025-10-07 15:47:24 +0200

    liten fix

--- Intermediate/Build/BuildRulesProjects/SpiritFrequencyModuleRules/obj/Development/SpiritFrequencyModuleRules.AssemblyInfo.cs
+++ Intermediate/Build/BuildRulesProjects/SpiritFrequencyModuleRules/obj/Development/SpiritFrequencyModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpiritFrequencyModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d2c77a7592a5c179288eb7473ed6edb7dd97d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fab30b4867e23ef6b1fd69f6aa6c8671bf9d77c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpiritFrequencyModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpiritFrequencyModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

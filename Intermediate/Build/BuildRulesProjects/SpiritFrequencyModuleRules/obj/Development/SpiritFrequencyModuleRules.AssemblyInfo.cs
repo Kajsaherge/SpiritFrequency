17c690c44aeb66f44ce719f234800fd740c89820
Nora Vidström <144795280+NoraVstrom@users.noreply.github.com>
2026-01-06 14:10:37 +0100

    menyn är fixad

--- Intermediate/Build/BuildRulesProjects/SpiritFrequencyModuleRules/obj/Development/SpiritFrequencyModuleRules.AssemblyInfo.cs
+++ Intermediate/Build/BuildRulesProjects/SpiritFrequencyModuleRules/obj/Development/SpiritFrequencyModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpiritFrequencyModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8396461555cd1dc8904dc33445d6c8d1d265197e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c171476fdccd9b9463503990d120c955e947ea4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpiritFrequencyModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpiritFrequencyModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
